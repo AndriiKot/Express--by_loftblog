ab0daa392e0c5c470b73e6d8a81215a692449a58
AndriiKot <a_kocyuba@ukr.net>
2024-10-23 15:07:51 +0200

    Express 'Express--by_loftblot' ! testing router './' and 'artists' !

--- server.js
+++ server.js
@@ -2,6 +2,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const artists = [
+  {
+    id: 1,
+    name: "Metallica",
+    genre: "Heavy Metal",
+  },
+  {
+    id: 2,
+    name: "AC/DC",
+    genre: "Hard Rock",
+  },
+  {
+    id: 3,
+    name: "Nirvana",
+    genre: "Grunge",
+  },
+];
+
+app.get("/artists", (req, res) => {
+  res.send(artists);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello Express testing!!!!");
 });
